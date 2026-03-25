26dd55b6345182a809d53bc0b7fe852426ee62e2
laraproto <laraproto@proton.me>
2026-03-25 21:17:41 +0200

    add shadcn, color theme and start working on stuff

---
+++ web/src/lib/components/site-header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import SidebarIcon from "@lucide/svelte/icons/sidebar";
+	import SearchForm from "./search-form.svelte";
+	import { Button } from "$lib/components/ui/button/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+	const sidebar = Sidebar.useSidebar();
+</script>
+
+<header class="bg-background sticky top-0 z-50 flex w-full items-center border-b">
+	<div class="flex h-(--header-height) w-full items-center gap-2 px-4">
+		<Button class="size-8" variant="ghost" size="icon" onclick={sidebar.toggle}>
+			<SidebarIcon />
+		</Button>
+		<Separator orientation="vertical" class="me-2 h-4" />
+		<SearchForm class="w-full sm:ms-auto sm:w-auto" />
+	</div>
+</header>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import './layout.css';
+	import { ModeWatcher } from "mode-watcher";
 	import favicon from '$lib/assets/favicon.svg';
+
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import SiteHeader from "$lib/components/site-header.svelte";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="[--header-height:calc(--spacing(14))]">
+	<Sidebar.Provider class="flex flex-col">
+		<SiteHeader />
+		<div class="flex flex-1">
+			<AppSidebar />
+			<Sidebar.Inset>
+				<div class="flex flex-1 flex-col p-4">
+					{@render children?.()}
+				</div>
+			</Sidebar.Inset>
+		</div>
+	</Sidebar.Provider>
+</div>
