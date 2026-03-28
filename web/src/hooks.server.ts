23c6d12595d1f016274926929ffd2f540aeca7ee
laraproto <laraproto@proton.me>
2026-03-28 17:44:39 +0200

    wtf

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -2,20 +2,26 @@ import authServer from '$lib/server/auth-server';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const session = await authServer.getSession();
+	try {
+		const session = await authServer.getSession();
 
-	if (session.error) {
-		console.error('Error fetching session:', session.error);
+		if (session.error) {
+			console.error('Error fetching session:', session.error);
+		}
+
+		if (session.data) {
+			event.locals.session = session.data.session;
+			event.locals.user = session.data.user;
+		} else {
+			event.locals.session = null;
+			event.locals.user = null;
+		}
+
+		const response = await resolve(event);
+		return response;
+	} catch (err) {
+		console.error('Error in handle function:', err);
+		const response = await resolve(event);
+		return response;
 	}
-
-	if (session.data) {
-		event.locals.session = session.data.session;
-		event.locals.user = session.data.user;
-	} else {
-		event.locals.session = null;
-		event.locals.user = null;
-	}
-
-	const response = await resolve(event);
-	return response;
 };
